c20f983d3feba558489682661fad9b46c6ee2f3a
Will Smith <wwsmith95@gmail.com>
2025-03-03 11:13:12 -0600

    added number selection grid

--- Sudoku.py
+++ Sudoku.py
@@ -1,9 +1,11 @@
 import pygame
 
-width = 650
+width = 810
 grid_width = 540
-height = 540
-blockSize = int(grid_width / 9)
+height = 580
+grid_height = 540
+sudokuBlockSize = int(grid_width / 9)
+numberSelectBlockSize = 70
 black = (0, 0, 0)
 
 pygame.init()
@@ -11,15 +13,22 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-def drawGrid():
-    for x in range(0, grid_width, blockSize):
-        for y in range(0, height, blockSize):
-            rect = pygame.Rect(x, y, blockSize, blockSize)
+def drawSudokuGrid():
+    for x in range(10, grid_width + 10, sudokuBlockSize):
+        for y in range(10, grid_height + 10, sudokuBlockSize):
+            rect = pygame.Rect(x, y, sudokuBlockSize, sudokuBlockSize)
+            pygame.draw.rect(screen, black, rect, 1)
+
+def drawNumberSelectionGrid():
+    for x in range(grid_width + 50, grid_width + numberSelectBlockSize * 3 + 50, numberSelectBlockSize):
+        for y in range(10, numberSelectBlockSize * 3 + 10, numberSelectBlockSize):
+            rect = pygame.Rect(x, y, numberSelectBlockSize, numberSelectBlockSize)
             pygame.draw.rect(screen, black, rect, 1)
 
 while running: 
     screen.fill('white')
-    drawGrid()
+    drawSudokuGrid()
+    drawNumberSelectionGrid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
