1ecf62faed71036be1fac6b085c1498c28e1da22
Will Smith <wwsmith95@gmail.com>
2025-04-01 15:01:35 -0500

    I like this shade of green better

--- Sudoku.py
+++ Sudoku.py
@@ -10,7 +10,7 @@ sudokuBlockSize = int(grid_width / 9)
 black = (0, 0, 0)
 red = (246, 159, 157)
 grey = (128, 128, 128)
-green = (0, 255, 0)
+green = (130, 201, 36)
 blue = (0, 0, 255)
 background = (208, 244, 245)
 
